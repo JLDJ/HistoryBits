fdcec41092bf76347be17219103d94746dd3e759
Brian Olson <Brian Olson>
2017-02-21 13:50:45 -0600

    Infinite Scrolling *Need to fix sanitization issue

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -12,11 +12,66 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'favorites.html'
 })
 export class FavoritesPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  items = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+   // for (let i = 0; i < 30; i++) {
+   //   this.items.push( this.items.length );
+  //}
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FavoritesPage');
+  }
+
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation, loading more items for favorites feed.');
+
+    setTimeout(() => {
+      for (let i = 0; i < 10; i++) {
+        this.items.push(`<ion-card class="favoritesCard">
+
+  <ion-item class=favoritesCard>
+    <ion-avatar item-left>
+      <img src="../assets/pictures/marty-avatar.png">
+    </ion-avatar>
+    <h2>Marty McFly</h2>
+    <p>November 5, 1955</p>
+  </ion-item>
+
+  <img src="../assets/pictures/carpic.png" >
+
+  <ion-card-content>
+    
+    <p>Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.</p>
+  </ion-card-content>
+
+  <ion-row>
+    <ion-col>
+      <button ion-button icon-left clear small (click)="liked()">
+        <ion-icon name="thumbs-up"></ion-icon>
+        <div> Likes {{likeCount}}</div>
+      </button>
+    </ion-col>
+    <ion-col>
+      <!-- May need a popover for comments -->
+      <button ion-button icon-left clear small>
+        <ion-icon name="text"></ion-icon>
+        <div>4 Comments</div>
+      </button>
+    </ion-col>
+    <ion-col center text-center>
+      <ion-note>
+        11h ago
+      </ion-note>
+    </ion-col>
+  </ion-row>
+</ion-card>`);
+      }
+
+      console.log('Async operation has ended, items loaded.');
+      infiniteScroll.complete();
+    }, 2000);
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,10 +7,63 @@ import { FormsModule } from '@angular/forms';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items = [];
   title = "Home";
   likeCount = 0;
   constructor(public navCtrl: NavController) {
+    //for (let i = 0; i < 30; i++) {
+     // this.items.push(``);
+ // }
+  }
 
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation, loading more items for new feed.');
+
+    setTimeout(() => {
+      for (let i = 0; i < 10; i++) {
+        this.items.push(`<ion-card class="homeCard">
+
+  <ion-item class="homeCard">
+    <ion-avatar item-left>
+      <img src="../assets/pictures/marty-avatar.png">
+    </ion-avatar>
+    <h2>Marty McFly</h2>
+    <p>November 5, 1955</p>
+  </ion-item>
+
+  <img src="../assets/pictures/carpic.png" >
+
+  <ion-card-content>
+    
+    <p>Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.</p>
+  </ion-card-content>
+
+  <ion-row>
+    <ion-col>
+      <button ion-button icon-left clear small (click)="liked()">
+        <ion-icon name="thumbs-up"></ion-icon>
+        <div> Likes {{likeCount}}</div>
+      </button>
+    </ion-col>
+    <ion-col>
+      <!-- May need a popover for comments -->
+      <button ion-button icon-left clear small>
+        <ion-icon name="text"></ion-icon>
+        <div>4 Comments</div>
+      </button>
+    </ion-col>
+    <ion-col center text-center>
+      <ion-note>
+        11h ago
+      </ion-note>
+    </ion-col>
+  </ion-row>
+</ion-card>`);
+      }
+
+      console.log('Async operation has ended. finished loading items for new feed.');
+      infiniteScroll.complete();
+    }, 2000);
   }
 
   liked(){
